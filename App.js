ebe7792fe482ab3d6c6cf3fc611809ffdf32a971
Covee <uniqtop91@gmail.com>
2018-08-27 16:16:12 +0900

    react native의 navigator 모듈로 location 받음

--- App.js
+++ App.js
@@ -8,12 +8,24 @@ export default class App extends React.Component {
   state = {
     isLoaded: false,
   };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          isLoaded: true
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
   render() {
     const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {!isLoaded ? (
+        {isLoaded ? (
           <Weather />
           ) : (
           <View style={styles.loading}>
