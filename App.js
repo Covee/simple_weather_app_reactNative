402a461cb6877111b8f94bc13eaeea851964b566
Covee <uniqtop91@gmail.com>
2018-08-27 17:09:24 +0900

    openWeatherMap api 받아서 필요한 json params를 setState 해줌

--- App.js
+++ App.js
@@ -3,26 +3,39 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from "./Weather";
 import { Ionicons } from "@expo/vector-icons";
 
+const API_KEY = '2ee89a92babc5df1e8f9fdccbddda109';
 
 export default class App extends React.Component {
   state = {
     isLoaded: false,
     error: null,
+    temperature: null,
+    name: null,
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true,
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude)
       },
       error => {
         this.setState({
           error: error
-        });
+        })
       }
     );
   }
+  _getWeather= (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true,
+      })
+    })  
+  }
+
   render() {
     const { isLoaded, error } = this.state;
     return (
